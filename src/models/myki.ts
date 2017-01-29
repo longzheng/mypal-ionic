c8d5062aad687bc9778b474abbb2bab4aa361682
Long Zheng <long.zheng@gmail.com>
2017-01-29 19:11:35 +1100

    Added getting/setting card type and status from string

--- src/models/myki.ts
+++ src/models/myki.ts
@@ -1,10 +1,16 @@
 export namespace Myki {
     export class Account {
+        loading: boolean = false;
         holder: string;
         cards: Array<Card>;
+
+        constructor() {
+            this.cards = []
+        }
     }
 
     export class Card {
+        loading: boolean = false;
         holder: string;
         id: string;
         type: CardType;
@@ -18,6 +24,44 @@ export namespace Myki {
         lastTransactionDate: Date;
         autoTopup: boolean;
         transactions: Array<Transaction>;
+
+        constructor() {
+            this.transactions = []
+        }
+
+        setType(type: string) {
+            switch (type) {
+                case 'Full Fare':
+                    this.type = CardType.FullFare;
+                    break;
+                case 'Concession':
+                    this.type = CardType.Concession;
+                    break;
+                case 'Children':
+                    this.type = CardType.Children;
+                    break;
+                case 'Seniors':
+                    this.type = CardType.Seniors;
+                    break;
+                default:
+                    throw new Error('Invalid card type')
+            }
+        }
+
+        typeToString(): string {
+            switch (this.type) {
+                case CardType.FullFare:
+                    return "Full fare";
+                case CardType.Concession:
+                    return "Concession";
+                case CardType.Children:
+                    return "Children";
+                case CardType.Seniors:
+                    return "Seniors";
+                default:
+                    return '';
+            }
+        }
     }
 
     export class Transaction {
