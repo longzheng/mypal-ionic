15d1bb6a60374159aeb2c7de0fbb20e74df9ba54
Long Zheng <long.zheng@gmail.com>
2017-02-05 19:52:33 +1100

    Updated splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.styleLightContent();
       Splashscreen.hide();
     });
   }
