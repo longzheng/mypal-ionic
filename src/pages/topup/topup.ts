11af6e292aa950957d58a965490a84ba3d0e8e43
Long Zheng <long.zheng@gmail.com>
2017-02-26 19:40:19 +1100

    Added form validation to topup screen

--- src/pages/topup/topup.ts
+++ src/pages/topup/topup.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ViewController, NavParams, AlertController } from 'ionic-angular';
 import { Myki } from '../../models/myki';
 import { MykiProvider } from '../../providers/myki';
@@ -11,14 +12,33 @@ export class TopupPage {
 
   public topupOptions: Myki.TopupOptions = new Myki.TopupOptions()
   public loading: boolean = false;
+  public formTopupMoney: FormGroup;
+  public formTopupPass: FormGroup;
 
   constructor(
     public viewCtrl: ViewController,
     public navParams: NavParams,
     public mykiProvider: MykiProvider,
     public alertCtrl: AlertController,
+    public formBuilder: FormBuilder,
   ) {
+    // get topup type from navigation parameter
     this.topupOptions.topupType = navParams.get('type')
+
+    // initialize form groups
+    this.formTopupMoney = formBuilder.group({
+      moneyAmount: ['10', Validators.compose([Validators.required, this.validateMoneyAmount])]
+    })
+
+    this.formTopupPass = formBuilder.group({
+      // field validation
+      passDuration: ['7', Validators.compose([Validators.required, this.validatePassDuration])],
+      zoneFrom: ['1', Validators.compose([Validators.required])],
+      zoneTo: ['2', Validators.compose([Validators.required])],
+    }, {
+        // form validators
+        validator: this.validateZones('zoneFrom', 'zoneTo')
+      })
   }
 
   ionViewDidLoad() {
@@ -73,6 +93,59 @@ export class TopupPage {
     return Array.apply(null, { length: 81 }).map(function (value, index) {
       return (index + 1);
     });
+  }
+
+  public canNext() {
+    return (this.isTopupMoney() && this.formTopupMoney.valid) || (this.isTopupPass() && this.formTopupPass.valid)
+  }
+
+  public next() {
+    // if either of the forms are invalid
+    if (
+      (this.isTopupMoney() && !this.formTopupMoney.valid)
+      || (this.isTopupPass() && !this.formTopupPass.valid)
+    ) {
+      // show error
+      let alert = this.alertCtrl.create({
+        title: 'Top up options error',
+        subTitle: 'Please correct the errors ',
+        buttons: ['OK']
+      })
+      alert.present()
+    }
+  }
+
+  private validatePassDuration(control: FormControl) {
+    if (
+      control.value < 7
+      || (control.value > 7 && control.value < 28)
+      || control.value > 365
+    )
+      return { invalidPassDuration: true }
+
+    return null
+  }
+
+  private validateMoneyAmount(control: FormControl) {
+    if (
+      control.value < 10
+      || control.value > 250
+    )
+      return { invalidMoneyAmount: true }
+
+    return null
+  }
+
+  private validateZones(from: string, to: string) {
+    return (group: FormGroup) => {
+      let zoneFrom = parseInt(group.controls[from].value)
+      let zoneTo = parseInt(group.controls[to].value)
+
+      if (zoneFrom > zoneTo)
+        return { invalidZones: true }
+
+      return null
+    }
   }
 
 }
