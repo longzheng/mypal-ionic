2dc51b79e2592e5d2962b8c32464ce8ea785aadf
Long Zheng <long.zheng@gmail.com>
2017-04-09 20:22:23 +1000

    Top up location map tweaks
    Default location Melbourne CBD
    Attempt to centre on user location at load
    Added loading state to map

--- src/pages/topup-map/topup-map.ts
+++ src/pages/topup-map/topup-map.ts
@@ -9,6 +9,8 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, CameraPosition, MarkerO
 })
 export class TopupMapPage {
 
+  loading: boolean = false;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -32,41 +34,61 @@ export class TopupMapPage {
       'gestures': {
         'tilt': false,
       },
-      'controller': {
-        'clustering': true,
-        'rendering': 'animated',
-        'algorithm': 'nonHierarchicalDistanceBasedAlgorithm'
+      'camera': {
+        'latLng': new LatLng(-37.8136, 144.9631),
+        'zoom': 17
       }
     });
 
     // listen to MAP_READY event
     // You must wait for this event to fire before adding something to the map or modifying it in anyway
-    map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
+    map.one(GoogleMapsEvent.MAP_READY).then(() => {
 
-    // load myki top up locations
-    this.http.get("https://www.ptv.vic.gov.au/tickets/myki/ef1d0f60a/xml-list")
-      .map(response => response.text())
-      .subscribe(data => {
-        if (data) {
-          let parser = new DOMParser();
-          let xmlData = parser.parseFromString(data.trim(), "application/xml");
-          let locations = xmlData.getElementsByTagName("d");
-          for (var i = 0; i < locations.length; i++) {
-            let location = locations[i];
-            let locationName = location.getElementsByTagName("N")[0].textContent
-            let locationLat = parseFloat(location.getElementsByTagName("Lt")[0].textContent)
-            let locationLng = parseFloat(location.getElementsByTagName("Lg")[0].textContent)
-            // create new marker
-            let markerOptions: MarkerOptions = {
-              position: new LatLng(locationLat, locationLng),
-              title: locationName
-            };
+      // center on current location
+      // wait a second for things to initialize a bit
+      setTimeout(() => {
+        map.getMyLocation().then((location) => {
+          map.setCenter(location.latLng)
+          map.setZoom(17)
+        });
+      }, 1000)
 
-            map.addMarker(markerOptions);
+      // start loading throbber
+      this.loading = true
+
+      // load myki top up locations
+      this.http.get("https://www.ptv.vic.gov.au/tickets/myki/ef1d0f60a/xml-list")
+        .map(response => response.text())
+        .subscribe(
+        data => {
+          if (data) {
+            let parser = new DOMParser();
+            let xmlData = parser.parseFromString(data.trim(), "application/xml");
+            let locations = xmlData.getElementsByTagName("d");
+            for (var i = 0; i < locations.length; i++) {
+              let location = locations[i];
+              let locationName = location.getElementsByTagName("N")[0].textContent
+              let locationLat = parseFloat(location.getElementsByTagName("Lt")[0].textContent)
+              let locationLng = parseFloat(location.getElementsByTagName("Lg")[0].textContent)
+              // create new marker
+              let markerOptions: MarkerOptions = {
+                position: new LatLng(locationLat, locationLng),
+                title: locationName
+              };
+
+              map.addMarker(markerOptions);
+            }
           }
+        }, error => {
+          // no op
+        }, () => {
+          this.loading = false;
+        });
 
-        }
-      });
+
+    });
+
+
   }
 
 }
