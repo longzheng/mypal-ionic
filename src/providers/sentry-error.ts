143ac55a08c94b524581d4a8cbe0a48d40f834b6
Long Zheng <long.zheng@gmail.com>
2017-04-09 13:21:41 +1000

    Disable error logging in dev

--- src/providers/sentry-error.ts
+++ src/providers/sentry-error.ts
@@ -1,9 +1,11 @@
 import { IonicErrorHandler } from 'ionic-angular';
 import Raven from 'raven-js';
 
-Raven
-    .config('https://54c01f896cbb4594b75639dfbf59b81e@sentry.io/155883')
-    .install();
+// only initialize Sentry.io if on actual device
+if ((<any>window).cordova)
+    Raven
+        .config('https://54c01f896cbb4594b75639dfbf59b81e@sentry.io/155883')
+        .install();
 
 // Sentry.io error handling
 export class SentryErrorHandler extends IonicErrorHandler {
